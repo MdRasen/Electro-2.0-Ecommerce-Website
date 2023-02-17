9401e053f9a5e341e3f4b4cd32b913d35cd8d607
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2023-02-17 22:31:40 +0600

    Project setup completed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* 
+Project Name: Electro 2.0 Ecommerce Website
+Description: A complete responsive e-commerce website design
+             by using HTML CSS and JavaScript from scratch.
+Author: Md Al Amin Hossen
+Github: https://github.com/MdRasen
+License: MIT License
+Copyright: 2023 ©MdRasen
+*/
