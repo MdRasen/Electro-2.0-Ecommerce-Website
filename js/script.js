b04c716b3dc80cef461de8357b5c16bee214d9fd
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2023-02-21 20:33:07 +0600

    Trending section completed

--- js/script.js
+++ js/script.js
@@ -7,3 +7,42 @@ Github: https://github.com/MdRasen
 License: MIT License
 Copyright: 2023 ©MdRasen
 */
+
+// Product Swiper Start
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  // For autoplay feature
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
+
+// Product Swiper End
